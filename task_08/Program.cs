8853587776b55b2860304e330fb710e137fa0b57
70by666 <70bytriplesix666@gmail.com>
2023-01-14 00:02:44 +0300

    added task_08

--- task_08/Program.cs
+++ task_08/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе 
+// показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+System.Console.WriteLine("input number ");
+int num = System.Convert.ToInt32(System.Console.ReadLine());
+
+int i = 0;
+while (i < num - 1) {
+    i += 2;
+    System.Console.Write($"{i},");
+}
